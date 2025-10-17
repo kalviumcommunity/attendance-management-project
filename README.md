1930e0cd6b142d726b6d5145048f50af95ddd741
Mangalam Kumar <mangalamkumar2002@gmail.com>
2025-10-17 17:54:56 +0530

    feat: Initial project setup (Part 1)

---
+++ README.md
@@ -0,0 +1,9 @@
+### Commands to run
+1. Compile the Code: 
+    ```bash
+    javac src/com/school/Main.java
+    ```
+2. Run the program:
+    ```bash
+    java -cp src com.school.Main
+    ```
--- src/com/school/Main.java
+++ src/com/school/Main.java
@@ -1,7 +1,8 @@
-package com.school;
+package com.school;  // Declares that this file belongs to the 'com.school' package.
 
 public class Main{
     public static void main(String args[]){
-        System.out.println("Welcome");
+        System.out.println("Welcome to the School Attendance System Project!");
+        System.out.println("Session 1: Project Setup and Orientation Complete");
     }
 }
